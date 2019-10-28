5dc406c2cdfee72435105165089eaa6ebc62f535
amirahmadadibi <amirahmadadibi@gmail.com>
2019-10-28 15:42:15 +0330

    simple usage of kotlin corroutines with two susped functions

--- app/src/main/java/com/amirahmadadibi/projects/androidcoroutine/MainActivity.kt
+++ app/src/main/java/com/amirahmadadibi/projects/androidcoroutine/MainActivity.kt
@@ -2,11 +2,50 @@ package com.amirahmadadibi.projects.androidcoroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-
+    var i = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonCo.setOnClickListener {
+            Log.d("tagxx", "onClick : launched Coroutine")
+            CoroutineScope(IO).launch {
+                getResultOneFromApi()
+            }
+        }
+    }
+
+    private suspend fun fakeAPi():String{
+        return getResultOneFromApi()
+    }
+
+    private suspend fun getResultOneFromApi():String {
+        i++
+        delay(1000)
+
+        withContext(Main) {
+            textViewStatus.text = "job one done! #$i"
+        }
+
+        getResultTwoFromApi()
+        i++
+        withContext(Main){
+            textViewStatus.text  = "job two done! #$i"
+        }
+
+        return "#Job number one done!"
+    }
+
+    private suspend fun getResultTwoFromApi() {
+        delay(2000)
     }
 }
