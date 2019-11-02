c3f75f33d586d5afc00eba880d65781eb0ca451d
amirahmadadibi <amirahmadadibi@gmail.com>
2019-11-02 12:55:28 +0330

    makeing activity an scope

--- app/src/main/java/com/amirahmadadibi/projects/androidcoroutine/CoroutineNetwroking.kt
+++ app/src/main/java/com/amirahmadadibi/projects/androidcoroutine/CoroutineNetwroking.kt
@@ -2,44 +2,58 @@ package com.amirahmadadibi.projects.androidcoroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_coroutine_netwroking.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import kotlin.coroutines.CoroutineContext
 
-class CoroutineNetwroking : AppCompatActivity() {
+class CoroutineNetwroking() : AppCompatActivity(), CoroutineScope {
+    private lateinit var job: Job
+    //makeing a wo
+    override val coroutineContext: CoroutineContext
+        get() = Main + job
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutine_netwroking)
+        job = Job()
         button.setOnClickListener {
-            GlobalScope.launch(Default) {
+            val a = GlobalScope.async(IO) {
                 makeCall()
             }
+
+            launch {
+
+            }
+
+        }
+        //create an independent scope and doing the work
+        CoroutineScope(IO).launch {
+
         }
     }
 
 
-    suspend fun makeCall(){
+    suspend fun makeCall(): String {
         var client = OkHttpClient()
-        val request = Request.Builder().
-            url("https://jsonplaceholder.typicode.com/todos/1")
+        val request = Request.Builder().url("https://jsonplaceholder.typicode.com/todos/1")
             .build()
 
         val response = client.newCall(request).execute()
-        Log.d("tagxx", response.body!!.string())
 
-        withContext(Main){
-            Toast.makeText(this@CoroutineNetwroking,"done",Toast.LENGTH_SHORT).show()
+        withContext(Main) {
+            Toast.makeText(this@CoroutineNetwroking, "done", Toast.LENGTH_SHORT).show()
         }
+        return response.body!!.string()
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
